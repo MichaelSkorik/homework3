6ac9c7800d321d1a6be276e161d68e1693d7892d
Michael <Michael@SKorik.com>
2023-07-14 15:09:04 +0300

    Ex_23

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <=num; i++);
+{
+    int ans = i * i * i;
+    Console.WriteLine(ans);
+}
